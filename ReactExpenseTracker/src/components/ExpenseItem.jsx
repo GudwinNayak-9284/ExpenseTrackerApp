d36fa1caf7c5274671046effc9f69e4abc936de4
gudwin <gudwinnayak786@gmail.com>
2023-05-21 12:13:19 +0530

    deleteItem button event added

--- ReactExpenseTracker/src/components/ExpenseItem.jsx
+++ ReactExpenseTracker/src/components/ExpenseItem.jsx
@@ -1,13 +1,35 @@
 
 const ExpenseItem = () => {
+
+  
+    let date=new Date()
+    // let currDate=date.getDate();
+    // let currMonth=date.getMonth()+1
+    // let currYear=date.getFullYear()
+    // let obj={
+    //   currDate,
+    //   currMonth,
+    //   currYear
+
+    const month=date.toLocaleString('en-Us',{month:"long"})
+    const day=date.toLocaleString('en-us',{day:'2-digit'})
+    const year=date.toLocaleString('en-us',{year:'numeric'})
+
+    function deleteItem(item){
+      console.log(`delete Item ${item}`)
+    }
   return (
     <div>
      <h1>Expense Item</h1>
      <div>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+     <div className="expense-item">
+            <div className="expense-item__date">{day}/{month}/{year}</div>
+            <div><h3>smart watch</h3></div>
+            <div className="expense-item__amount">$100</div>
+            <button onClick={deleteItem}>Delete item</button>
+        </div>
       </div>
+  
     </div>
   )
 }
